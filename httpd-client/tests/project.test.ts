9c39befe2086863c4789c590f92f9ca7c6adc276
Sebastian Martinez <me@sebastinez.dev>
2023-07-06 23:22:52 +0200

    httpd: Add missing httpd tests
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- httpd-client/tests/project.test.ts
+++ httpd-client/tests/project.test.ts
@@ -1,11 +1,14 @@
-import { describe, test } from "vitest";
-
 import { HttpdClient } from "../index";
 import {
   aliceMainHead,
   aliceRemote,
+  bobRemote,
+  cobRid,
   sourceBrowsingRid,
 } from "@tests/support/fixtures";
+import { authenticate } from "./support/httpd";
+import { describe, test, expect } from "vitest";
+import { isMatch } from "lodash";
 
 const api = new HttpdClient({
   hostname: "127.0.0.1",
@@ -76,12 +79,195 @@ describe("project", () => {
     await api.project.getCommitBySha(sourceBrowsingRid, aliceMainHead);
   });
 
-  test.todo("#getDiff(id, revisionBase, revisionOid)");
-  test.todo("#getIssueById(id, issueId)");
-  test.todo("#getAllIssues(id)");
-  test.todo("#createIssue(id, { title, description, assignees, tags })");
-  test.todo("#updateIssue(id, issueId, issueUpdateAction, authToken)");
-  test.todo("#getPatchById(id, patchId)");
-  test.todo("#getAllPatches(id)");
-  test.todo("#updatePatch(id, patchId, patchUpdateAction, authToken)");
+  test("#getDiff(id, revisionBase, revisionOid)", async () => {
+    await api.project.getDiff(
+      sourceBrowsingRid,
+      "90f6d058ece12f75f349bc7bbe88142187fe0379",
+      aliceMainHead,
+    );
+  });
+
+  test("#getIssueById(id, issueId)", async () => {
+    await api.project.getIssueById(
+      cobRid,
+      "4fc727e722d3979fd2073d9b56b2751658a4ae79",
+    );
+  });
+
+  test("#getAllIssues(id)", async () => {
+    await api.project.getAllIssues(cobRid, {
+      page: 0,
+      perPage: 5,
+      state: "open",
+    });
+  });
+
+  describe("issue", async () => {
+    const sessionId = await authenticate(api);
+
+    test("#createIssue(id, { title, description, assignees, tags })", async () => {
+      const { id: issueId } = await api.project.createIssue(
+        cobRid,
+        {
+          title: "aaa",
+          description: "bbb",
+          assignees: [],
+          tags: ["bug", "documentation"],
+        },
+        sessionId,
+      );
+      await assertIssue(issueId, {
+        title: "aaa",
+        discussion: [{ body: "bbb" }],
+        assignees: [],
+        tags: ["bug", "documentation"],
+      });
+    });
+
+    test("#updateIssue(id, issueId, { type: 'edit' }, authToken)", async () => {
+      const issueId = await createIssueToBeModified(api, sessionId);
+      await api.project.updateIssue(
+        cobRid,
+        issueId,
+        { type: "edit", title: "ccc" },
+        sessionId,
+      );
+      await assertIssue(issueId, { title: "ccc" });
+    });
+
+    test("#updateIssue(id, issueId, { type: 'tag' }, authToken)", async () => {
+      const issueId = await createIssueToBeModified(api, sessionId);
+      await api.project.updateIssue(
+        cobRid,
+        issueId,
+        { type: "tag", add: ["bug"], remove: [] },
+        sessionId,
+      );
+      await assertIssue(issueId, { tags: ["bug"] });
+    });
+
+    test("#updateIssue(id, issueId, { type: 'assign' }, authToken)", async () => {
+      const issueId = await createIssueToBeModified(api, sessionId);
+      const assignee = bobRemote.replace("did:key:", "");
+      await api.project.updateIssue(
+        cobRid,
+        issueId,
+        {
+          type: "assign",
+          add: [assignee],
+          remove: [],
+        },
+        sessionId,
+      );
+      await assertIssue(issueId, { assignees: [`did:key:${assignee}`] });
+    });
+
+    test("#updateIssue(id, issueId, { type: 'lifecycle' }, authToken)", async () => {
+      const issueId = await createIssueToBeModified(api, sessionId);
+      await api.project.updateIssue(
+        cobRid,
+        issueId,
+        { type: "lifecycle", state: { status: "closed", reason: "solved" } },
+        sessionId,
+      );
+      await assertIssue(issueId, {
+        state: { status: "closed", reason: "solved" },
+      });
+    });
+  });
+
+  test("#getPatchById(id, patchId)", async () => {
+    await api.project.getPatchById(
+      cobRid,
+      "013f8b2734df1840b2e33d52ff5632c8d66b199a",
+    );
+  });
+
+  test("#getAllPatches(id)", async () => {
+    await api.project.getAllPatches(cobRid);
+  });
+
+  describe("patch", async () => {
+    const sessionId = await authenticate(api);
+
+    test("#createPatch(id, patchCreate, authToken)", async () => {
+      const { id: oid } = await api.project.createPatch(
+        cobRid,
+        {
+          title: "ppp",
+          description: "qqq",
+          target: "d7dd8cecae16b1108234e09dbdb5d64ae394bc25",
+          oid: "38c225e2a0b47ba59def211f4e4825c31d9463ec",
+          tags: [],
+        },
+        sessionId,
+      );
+      await assertPatch(oid, {
+        title: "ppp",
+        state: { status: "open" },
+        target: "delegates",
+        tags: [],
+        revisions: [
+          {
+            description: "qqq",
+            base: "d7dd8cecae16b1108234e09dbdb5d64ae394bc25",
+            oid: "38c225e2a0b47ba59def211f4e4825c31d9463ec",
+          },
+        ],
+      });
+    });
+
+    test("#updatePatch(id, patchId, { type: 'edit' }, authToken)", async () => {
+      const patchId = await createPatchToBeModified(api, sessionId);
+      await api.project.updatePatch(
+        cobRid,
+        patchId,
+        { type: "tag", add: ["bug"], remove: [] },
+        sessionId,
+      );
+      await assertPatch(patchId, {
+        tags: ["bug"],
+      });
+    });
+  });
 });
+
+async function createIssueToBeModified(api: HttpdClient, sessionId: string) {
+  const { id } = await api.project.createIssue(
+    cobRid,
+    { title: "aaa", description: "bbb", assignees: [], tags: [] },
+    sessionId,
+  );
+
+  return id;
+}
+
+async function createPatchToBeModified(api: HttpdClient, sessionId: string) {
+  const { id } = await api.project.createPatch(
+    cobRid,
+    {
+      title: "rrr",
+      description: "ttt",
+      target: "d7dd8cecae16b1108234e09dbdb5d64ae394bc25",
+      oid: "38c225e2a0b47ba59def211f4e4825c31d9463ec",
+      tags: [],
+    },
+    sessionId,
+  );
+
+  return id;
+}
+
+async function assertIssue(oid: string, change: Record<string, any>) {
+  expect(
+    //@prettier-ignore looks more readable than what prettier suggests.
+    isMatch(await api.project.getIssueById(cobRid, oid), change),
+  ).toBe(true);
+}
+
+async function assertPatch(oid: string, change: Record<string, any>) {
+  expect(
+    //@prettier-ignore looks more readable than what prettier suggests.
+    isMatch(await api.project.getPatchById(cobRid, oid), change),
+  ).toBe(true);
+}
--- httpd-client/tests/session.test.ts
+++ httpd-client/tests/session.test.ts
@@ -1,7 +1,23 @@
+import { HttpdClient } from "../index";
 import { describe, test } from "vitest";
+import { authenticate } from "./support/httpd";
+
+const api = new HttpdClient({
+  hostname: "127.0.0.1",
+  port: 8080,
+  scheme: "http",
+});
 
 describe("session", () => {
-  test.todo("#getById(id)");
+  test("#getById(id)", async () => {
+    const id = await authenticate(api);
+    await api.session.getById(id);
+  });
+
   test.todo("#update(id, {sig, pk})");
-  test.todo("#delete(id)");
+
+  test("#delete(id)", async () => {
+    const id = await authenticate(api);
+    await api.session.delete(id);
+  });
 });
